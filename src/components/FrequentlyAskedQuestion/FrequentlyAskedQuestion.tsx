12d098ad65fe7ae941f9f7bc7ef8566b582018d0
Sagar Khadka <sagarkhadka2069@gmail.com>
2023-03-26 20:50:00 +0545

    Add faq footer section

--- src/components/FrequentlyAskedQuestion/FrequentlyAskedQuestion.tsx
+++ src/components/FrequentlyAskedQuestion/FrequentlyAskedQuestion.tsx
@@ -1,4 +1,6 @@
-import { Accordion, Heading } from "@chakra-ui/react";
+import { Accordion, Heading, IconButton } from "@chakra-ui/react";
+import { BsQuestionSquareFill } from "react-icons/bs";
+import { ImBubbles } from "react-icons/im";
 import { faqArray } from "../../data/faq";
 import FaqAccordion from "../FaqAccordion/FaqAccordion";
 import "./FrequentlyAskedQuestion.css";
@@ -18,6 +20,38 @@ export default function FrequentlyAskedQuestion() {
               />
             ))}
           </Accordion>
+        </div>
+        <div className='faq_footer_section'>
+          <div className='frequently_asked_question'>
+            <IconButton
+              className='questions_icon_button'
+              aria-label='questions'
+              icon={<BsQuestionSquareFill />}
+              size='sm'
+              fontSize='24px'
+              variant='unstyled'
+              color='#ab141d'
+            />
+            <a href='#' style={{ textDecoration: "underline" }}>
+              More Frequently Asked Question
+            </a>
+          </div>
+          <div className='contact_us_section'>
+            <IconButton
+              className='chats_icon_button'
+              aria-label='chats'
+              icon={<ImBubbles />}
+              size='sm'
+              fontSize='24px'
+              marginLeft='6'
+              marginRight='2'
+              variant='unstyled'
+              color='#ab141d'
+            />
+            <a href='#' style={{ textDecoration: "underline" }}>
+              Contact us
+            </a>
+          </div>
         </div>
       </div>
     </div>
