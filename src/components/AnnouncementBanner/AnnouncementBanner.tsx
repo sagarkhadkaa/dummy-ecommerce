98121099109d1af8cd651bf749e8b9b1edc2c0a6
Sagar Khadka <sagarkhadka2069@gmail.com>
2023-03-27 09:08:42 +0545

    Hide the announcement section on click

--- src/components/AnnouncementBanner/AnnouncementBanner.tsx
+++ src/components/AnnouncementBanner/AnnouncementBanner.tsx
@@ -1,20 +1,32 @@
 import { IconButton } from "@chakra-ui/react";
+import { useState } from "react";
 import { BsChatDotsFill } from "react-icons/bs";
 import "./AnnouncementBanner.css";
+
 export default function AnnouncementBanner() {
+  const [visible, setVisible] = useState(true);
+
+  const handleBannerClick = () => {
+    setVisible(false);
+  };
+
   return (
-    <div className='announcement_banner'>
-      <div>Subscribe & Save + a personal shopper via text</div>
-      <IconButton
-        className='chat_icon_button'
-        aria-label='chat'
-        icon={<BsChatDotsFill />}
-        size='sm'
-        fontSize='16px'
-        paddingLeft='2'
-        variant='unstyled'
-      />
-      <div> &nbsp; every month --&gt;</div>
+    <div onClick={handleBannerClick}>
+      {visible && (
+        <div className='announcement_banner'>
+          <div>Subscribe & Save + a personal shopper via text</div>
+          <IconButton
+            className='chat_icon_button'
+            aria-label='chat'
+            icon={<BsChatDotsFill />}
+            size='sm'
+            fontSize='16px'
+            paddingLeft='2'
+            variant='unstyled'
+          />
+          <div> &nbsp; every month --&gt;</div>
+        </div>
+      )}
     </div>
   );
 }
